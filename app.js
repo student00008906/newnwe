149534bb20d70a168e7e0867e0a82c3355be0a56
ACER <you@example.com>
2021-04-07 13:10:25 +0500

    todo creater

--- app.js
+++ app.js
@@ -1,1 +1,69 @@
 
+// third party libs
+const express = require('express')
+const app = express()
+
+//node libs
+const fs = require('fs')
+const PORT = 8000
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({ extended:false}))
+
+app.get('/',(req,res) =>{
+    fs.readFile('./data/todos.json', (err, data) =>{
+        if (err) throw (err)
+    })
+
+    res.render('home')
+})
+
+app.post('/add', (req,res) =>{
+    const formData = req.body
+
+    if (formData.todo.trim()==''){
+        res.render('home',{error: true})
+    } else {
+        fs.readFile(
+            './data/todos.json',(err,data) =>{
+            if (err) thow (err)
+
+            const todos = JSON.parse(data)
+
+            const todo = {
+                id: id(),
+                desctiption: formData.todo,
+                done:false
+
+            }
+
+            todos.push(todo)
+
+            fs.writeFile(
+                './data/todos.json', JSON.stringify(todos, (err) =>{
+                if (err) throw (err)
+
+                fs.readFile('./data/todos.json', (err, data) =>{
+                    if (err) throw (err)
+
+                    const todos = JSON.parse(data)
+
+                    res.render('home',{ success: true})
+                })
+            }))
+        })
+    }
+})
+
+//http:localhost:8000
+app.listen(PORT, (err) =>{
+    if (err) thow (err)
+
+    console.log(`This app is running on port ${ PORT}`)
+})
+
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
